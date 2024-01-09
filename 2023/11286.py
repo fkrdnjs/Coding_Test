e29ffd438214c63fdc330cc4910b8a5f5a0e7f98
Paradise <68600918+fkrdnjs@users.noreply.github.com>
2024-01-09 14:08:22 +0900

    절댓값 힙

---
+++ 2023/11286.py
@@ -0,0 +1,19 @@
+import sys
+import heapq
+
+n = int(input())
+heap = []
+
+for i in range(n):
+    x = int(sys.stdin.readline())
+
+    if x != 0:
+        if x < 0:
+            heapq.heappush(heap, (-x, x))
+        else:
+            heapq.heappush(heap, (x, x))
+    else:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
